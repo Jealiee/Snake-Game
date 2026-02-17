1388e9e948dd4dbcbd2749d2cce1ad190d2b62db
Jealie <adamus_julia@wp.pl>
2026-02-17 16:39:35 +0100

    Never stopping squares

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import pygame
-from pygame import *
+from snake import Snake
+from grid import Grid
+
+FPS = 40
+DT = 1 / FPS
+
 
 class App:
-
     def __init__(self):
         self._running = True
         self._display_surf = False
         self.size = self.width, self.height = 1280, 720
-        self.player_position = pygame.Vector2(self.width/2, self.height/2)
-        self.snake_module_size = 40
+        self.snake = Snake(pygame.Vector2(self.width / 2, self.height / 2))
+        self.clock = pygame.time.Clock()
 
     def on_init(self):
         pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
-    
+        self._display_surf = pygame.display.set_mode(
+            self.size, pygame.HWSURFACE | pygame.DOUBLEBUF
+        )
+        return True
 
     def on_event(self, event):
 
@@ -21,32 +27,26 @@ class App:
             self._running = False
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                self.player_position[0] -= self.snake_module_size
-            elif event.key == pygame.K_RIGHT:
-                self.player_position[0] += self.snake_module_size
-            elif event.key == pygame.K_DOWN:
-                self.player_position[1] -= self.snake_module_size
-            elif event.key == pygame.K_UP:
-                self.player_position[1] += self.snake_module_size
+            self.snake.snake_movement(event)
 
     def on_execute(self):
 
-        if self.on_init() == False:
+        if not self.on_init():
             self._running = False
 
-        while (self._running):
-
+        while self._running:
             for event in pygame.event.get():
                 self.on_event(event)
 
-            self._display_surf.fill('black')
+            self._display_surf.fill((0,0,0))
 
-            emerald = (80, 220, 100)
-            snake_head = pygame.draw.rect(self._display_surf, emerald, (self.player_position[0], self.player_position[1],self.snake_module_size,self.snake_module_size))            
-            
+            self.snake.update_snake(DT)
+            self.snake.draw_snake(self._display_surf)
+
             pygame.display.flip()
+            self.clock.tick(FPS)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     theApp = App()
     theApp.on_execute()
---
+++ snake.py
@@ -0,0 +1,35 @@
+import pygame
+
+
+class Snake:
+    def __init__(self, player_pos):
+        self.module_size = 40
+        self.player_position = player_pos
+        self.direction = pygame.K_RIGHT
+
+    def snake_movement(self, event):
+        if event.type == pygame.KEYDOWN:
+            self.direction = event.key
+
+    def draw_snake(self, display_surf):
+        emerald = (80, 220, 100)
+        self.snake_head = pygame.draw.rect(
+            display_surf,
+            emerald,
+            (
+                self.player_position[0],
+                self.player_position[1],
+                self.module_size,
+                self.module_size,
+            ),
+        )
+
+    def update_snake(self, dt):
+        if self.direction == pygame.K_RIGHT:
+            self.player_position[0] += self.module_size * dt
+        elif self.direction == pygame.K_LEFT:
+            self.player_position[0] -= self.module_size * dt
+        elif self.direction == pygame.K_DOWN:
+            self.player_position -= self.module_size * dt
+        elif self.direction == pygame.K_UP:
+            self.player_position += self.module_size * dt
